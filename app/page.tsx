3dcea52d771fc7e6cfe740ac6fa9d34b95f5373e
carlamonaliza9-rgb <carlamonaliza9@gmail.com>
2026-04-30 13:55:08 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      fontFamily: "Arial"
+    }}>
+      <h1>Sistema de Gestão Escolar</h1>
+      <p>Seu sistema está funcionando 🚀</p>
+    </div>
+  )
+}
